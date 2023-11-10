e79fbca3a5c6c51386f81ffb77c6a695e3923c68
Kurio <61805275+Kuriioo@users.noreply.github.com>
2023-11-10 15:46:04 +0100

    Add files via upload

--- src/CLI/main.cpp
+++ src/CLI/main.cpp
@@ -4,6 +4,7 @@
 */
 
 #include <Kafe/VM/VM.hpp>
+#include <Kafe/Kafe.hpp>
 #include <CLI/Repl.hpp>
 
 #include <iostream>
@@ -12,11 +13,42 @@
 int main (int argc, char **argv)
 {
   int result { 0 };
-  /*std::string file;
-  Kafe::VM vm {};
-  result = vm.run();*/
-  Kafe::Repl repl { };
-  result = repl.run();
-   
+  enum cmd_mode
+  {
+    COMPILE,
+    REPL,
+    NONE
+  };
+  cmd_mode mode { cmd_mode::NONE };
+  const std::string arguments[] 
+  {
+    "--help", // show help message
+    "-o",     // sets output file
+    "-c",     // compile program
+  };
+  //TODO: catch arguments in the command
+  switch (mode)
+  {
+    case cmd_mode::NONE:
+    {
+      std::printf(
+       "Kafe Version %i.%i\n"
+       "kafe --help \t; to show the help message.\n",
+       KAFE_VERSION_MAJOR,
+       KAFE_VERSION_MINOR
+      );
+      break;
+    };
+    case cmd_mode::COMPILE:
+    {
+      break;
+    };
+    case cmd_mode::REPL:
+    {
+      Kafe::Repl repl { };
+      result = repl.run();    
+      break;
+    };
+  };
   return result;
 }
